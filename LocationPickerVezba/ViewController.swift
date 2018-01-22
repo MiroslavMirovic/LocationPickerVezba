7683ab1fdced2dfb1eef4a678e58d026452cdf85
Miroslav Mirovic <miroslav.mirovic@gmail.com>
2018-01-22 13:30:09 +0100

    Initial Commit

---
+++ LocationPickerVezba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationPickerVezba
+//
+//  Created by Miroslav Mirovic on 1/22/18.
+//  Copyright © 2018 Miroslav Mirovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
